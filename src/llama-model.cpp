eeb862aa25bbc8423be390ac3a6b9afbba52c897
vektorprime <vektorprime@gmail.com>
2026-06-16 23:21:05 -0500

    fix: replace sizeof(block_df11) with t->nb[0] to avoid missing include

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -1464,12 +1464,12 @@ bool llama_model_base::load_tensors(llama_model_loader & ml) {
                         const int64_t old_nb1 = t->nb[1];
                         t->nb[1] = (int64_t)((actual - row0 + t->ne[1] - 2) / (t->ne[1] - 1));
                         if (ggml_custom_logs_enabled()) {
+                            const size_t nbytes_before = (size_t)t->nb[0] + (t->ne[0]-1)*t->nb[0] + (t->ne[1]-1)*old_nb1 + (t->ne[2]-1)*t->nb[2] + (t->ne[3]-1)*t->nb[3];
                             fprintf(stderr, "[DF11-debug] nb[1] shrink: %s ne=[%lld,%lld,%lld,%lld] row0=%lld actual=%zu old_nb1=%lld new_nb1=%lld ggml_nbytes_before=%zu ggml_nbytes_after=%zu\n",
                                     ggml_get_name(t),
                                     (long long)t->ne[0], (long long)t->ne[1], (long long)t->ne[2], (long long)t->ne[3],
                                     (long long)row0, actual, (long long)old_nb1, (long long)t->nb[1],
-                                    sizeof(block_df11) + (t->ne[0]-1)*t->nb[0] + (t->ne[1]-1)*old_nb1 + (t->ne[2]-1)*t->nb[2] + (t->ne[3]-1)*t->nb[3],
-                                    ggml_nbytes(t));
+                                    nbytes_before, ggml_nbytes(t));
                             fflush(stderr);
                         }
                     }
