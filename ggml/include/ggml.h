3b8cf3af8ccd94b9f88b1a29a2497223d2b2a961
vektorprime <vektorprime@gmail.com>
2026-06-16 22:30:36 -0500

    fix: restore GGML_TYPE_Q1_0=41 accidentally dropped from ggml.h enum

--- ggml/include/ggml.h
+++ ggml/include/ggml.h
@@ -432,6 +432,7 @@ extern "C" {
         // GGML_TYPE_IQ4_NL_8_8 = 38,
         GGML_TYPE_MXFP4   = 39, // MXFP4 (1 block)
         GGML_TYPE_NVFP4   = 40, // NVFP4 (4 blocks, E4M3 scale)
+        GGML_TYPE_Q1_0    = 41,
         GGML_TYPE_DF11    = 42,
         GGML_TYPE_COUNT   = 43,
     };
