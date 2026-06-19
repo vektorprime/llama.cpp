d6c56b814d624b14a898fa5fbc5c10bba50d88e1
Hermes Analysis <analysis@hermes.local>
2026-06-19 04:19:52 +0000

    fix: replace std::fabs with manual abs to fix lambda parse error

--- tools/perplexity/perplexity.cpp
+++ tools/perplexity/perplexity.cpp
@@ -1951,7 +1951,9 @@ static void kl_divergence(llama_context * ctx, const common_params & params) {
         std::vector<token_analysis> sorted = token_analyses;
         std::sort(sorted.begin(), sorted.end(),
             [](const token_analysis & a, const token_analysis & b) {
-                return std::fabs(a.p_diff) > std::fabs(b.p_diff);
+                float fa = a.p_diff < 0 ? -a.p_diff : a.p_diff;
+                float fb = b.p_diff < 0 ? -b.p_diff : b.p_diff;
+                return fa > fb;
             });
 
         std::string worst_path = params.analysis_csv + ".worst_tokens.txt";
